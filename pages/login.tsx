be3ecbfb90b7d6495870c7e9d304706cdd7aaaf6
Nguyen Tan Dat <datnguyen25082000@gmail.com>
2022-01-08 08:25:33 +0700

    NTDat - api login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
 import { MdFacebook } from "react-icons/md";
 import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
+import Router from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login: NextPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const notify = () => {
+    toast("Tên đăng nhập hoặc mật khẩu sai, Vui lòng nhập lại");
+  };
+
+  async function handleLogin() {
+    const loginInfo = {
+      identifier: username,
+      password: password,
+    };
+
+    const login = await fetch(`${process.env.URL_API}/auth/local`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginInfo),
+    });
+
+    const loginResponse = await login.json();
+
+    if (loginResponse && loginResponse.user && loginResponse.jwt) {
+      localStorage.setItem("user", loginResponse.user);
+      localStorage.setItem("jwt", loginResponse.jwt);
+
+      Router.push("/");
+    } else notify();
+  }
+
   return (
     <section className="vh-100 d-flex center">
       <div className="container-fluid h-custom">
@@ -16,7 +51,12 @@ const Login: NextPage = () => {
             />
           </div>
           <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <form>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleLogin();
+              }}
+            >
               <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
                 <p className="lead fw-normal mb-0 me-3">Sign in with</p>
                 <button
@@ -48,6 +88,7 @@ const Login: NextPage = () => {
               <div className="form-outline mb-4">
                 <input
                   type="email"
+                  onChange={(e) => setUsername(e.target.value)}
                   id="form3Example3"
                   className="form-control form-control-lg"
                   placeholder="Enter a valid email address"
@@ -61,6 +102,7 @@ const Login: NextPage = () => {
                 <input
                   type="password"
                   id="form3Example4"
+                  onChange={(e) => setPassword(e.target.value)}
                   className="form-control form-control-lg"
                   placeholder="Enter password"
                 />
@@ -87,7 +129,7 @@ const Login: NextPage = () => {
               </div>
 
               <div className="text-center text-lg-start mt-4 pt-2">
-                <button type="button" className="btn btn-primary btn-lg">
+                <button type="submit" className="btn btn-primary btn-lg">
                   Login
                 </button>
                 <p className="small fw-bold mt-2 pt-1 mb-0">
@@ -101,6 +143,7 @@ const Login: NextPage = () => {
           </div>
         </div>
       </div>
+      <ToastContainer limit={3}></ToastContainer>
     </section>
   );
 };
